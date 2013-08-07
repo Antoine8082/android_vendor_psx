6679b2a67aa62c0f79ae698134a0dd44e5bc598c
MrBaNkS <banksmi09@gmail.com>
2013-08-07 15:56:35 -0500

    Remove useless /system/media/video bloat

--- products/common_media_effects.mk
+++ products/common_media_effects.mk
@@ -4,6 +4,3 @@
 PRODUCT_COPY_FILES +=  \
     vendor/psx/proprietary/common/system/media/LMspeed_508.emd:system/media/LMspeed_508.emd \
     vendor/psx/proprietary/common/system/media/PFFprec_600.emd:system/media/PFFprec_600.emd
-
-# video files
-$(call inherit-product-if-exists, frameworks/base/data/videos/VideoPackage2.mk)
