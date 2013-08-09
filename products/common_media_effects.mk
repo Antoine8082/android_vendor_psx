cc830a8ca494749605db891ccd3d159cb38922cf
MrBaNkS <banksmi09@gmail.com>
2013-08-08 21:07:34 -0500

    We don't need you either

--- products/common_media_effects.mk
+++
@@ -1,6 +0,0 @@
-# Blobs necessary for media effects
-
-# media effects
-PRODUCT_COPY_FILES +=  \
-    vendor/psx/proprietary/common/system/media/LMspeed_508.emd:system/media/LMspeed_508.emd \
-    vendor/psx/proprietary/common/system/media/PFFprec_600.emd:system/media/PFFprec_600.emd
