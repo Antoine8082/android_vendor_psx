0d2f2740743f98212a6fb4ad2410b528b00c8591
MrBaNkS <banksmi09@gmail.com>
2013-08-24 16:20:51 -0500

    Update more build fingerprints

--- products/psx_grouper.mk
+++ products/psx_grouper.mk
@@ -11,4 +11,4 @@ PRODUCT_DEVICE := grouper
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := Asus
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:4.2.2/JDQ39/573038:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 4.2.2 JDQ39 573038 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:4.3/JWR66Y/776638:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 4.3 JWR66Y 776638 release-keys"
